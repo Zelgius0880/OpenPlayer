09b13e0a147c61f93266007d26fbd5ac32b10a50
florian <f.dupierreux@side.eu>
2020-10-02 14:31:30 +0200

    gradle task for deploying server

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 import java.util.Properties
 import java.io.FileInputStream
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.hidetake.groovy.ssh.core.RunHandler
+import org.hidetake.groovy.ssh.session.SessionHandler
+import org.hidetake.groovy.ssh.core.Remote
+import org.hidetake.groovy.ssh.core.Service
 
 buildscript {
 
@@ -35,14 +40,38 @@ tasks.register("clean", Delete::class.java) {
 
 val getProps by extra {
     fun(propName: String): String {
-        val propsFile = rootProject.file("local.properties")
-        return if (propsFile.exists()) {
-            val props = Properties()
-            props.load(FileInputStream(propsFile))
-            props[propName] as String
-        } else {
-            ""
+        return getProps(propName)
+    }
+}
+plugins {
+    id("org.hidetake.ssh") version "2.10.1"
+}
+
+val raspberry = remotes.create("raspberry") {
+    host = "192.168.1.63"
+    user = "pi"
+    password = getProps("password", "gradle.local.properties")
+}
+
+val deployServer = tasks.create("deployServer") {
+    doLast {
+        ssh.runSessions {
+            session(raspberry) {
+                put(File("media-server"), File("/home/pi/")){it.name != "node_modules"}
+                logger.warn(execute("pm2 restart media-server"))
+            }
         }
+    }
+}
+
+fun getProps(propName: String, path: String = "local.properties"): String {
+    val propsFile = rootProject.file(path)
+    return if (propsFile.exists()) {
+        val props = Properties()
+        props.load(FileInputStream(propsFile))
+        props[propName] as String
+    } else {
+        ""
     }
 }
 
@@ -90,3 +119,12 @@ val generateBuildConfig by extra {
         }
     }
 }
+
+fun Service.runSessions(action: RunHandler.() -> Unit) =
+    run(delegateClosureOf(action))
+
+fun RunHandler.session(vararg remotes: Remote, action: SessionHandler.() -> Unit) =
+    session(*remotes, delegateClosureOf(action))
+
+fun SessionHandler.put(from: Any, into: Any, filter: (f: File) -> Boolean = {true}) =
+    put(hashMapOf("from" to from, "into" to into, "filter" to filter))
